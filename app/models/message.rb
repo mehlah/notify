f84931168ba28ed8b21741243b591a3e50c1b524
Mehdi Lahmam <mehdi@craftsmen.io>
2014-09-18 03:17:10 +0200

    Show newest messages first

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,8 @@ class Message < ActiveRecord::Base
   belongs_to :group
 
   validates :body, presence: true
+
+  def self.ordered
+    order(created_at: :desc)
+  end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -4,4 +4,14 @@ describe Message do
   it { should belong_to(:group) }
 
   it { should validate_presence_of(:body) }
+
+  describe '.ordered' do
+    it 'returns newer messages first' do
+      create(:message, body: 'first', created_at: 1.days.ago)
+      create(:message, body: 'third', created_at: 3.day.ago)
+      create(:message, body: 'second', created_at: 2.days.ago)
+
+      expect(Message.ordered.pluck(:body)).to eq(%w(first second third))
+    end
+  end
 end
