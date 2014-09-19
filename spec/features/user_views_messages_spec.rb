32ef079c80959d395a56e7d0087ee31a0d8c5838
Mehdi Lahmam <mehdi@craftsmen.io>
2014-09-19 07:27:23 +0200

    Add missing feature spec for: User views a group messages

---
+++ spec/features/user_views_messages_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+feature 'User views a group messages' do
+  scenario 'views all the messages' do
+    user = create(:user, :with_groups)
+    login_as(user, scope: :user)
+    message = create(:message, group: user.groups.first)
+
+    visit '/'
+
+    within('#sent-messages') do
+      expect(page).to have_content(message.body)
+    end
+  end
+
+  scenario 'views a blankslate' do
+    user = create(:user, :with_groups)
+    login_as(user, scope: :user)
+
+    visit '/'
+
+    within('#sent-messages') do
+      expect(page).to have_content("Aucun message n'a encore été envoyé aux parents d'élèves de cette classe.")
+    end
+  end
+
+  scenario 'views the created_at date and time' do
+    user = create(:user, :with_groups)
+    login_as(user, scope: :user)
+    message = create(:message, group: user.groups.first, created_at: 2.hours.ago)
+
+    visit '/'
+
+    within('#sent-messages') do
+      expect(page).to have_content(l(message.created_at, format: :short))
+    end
+  end
+end
