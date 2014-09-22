9a7085eafc0fb5aef6bdac589ea950aff93b0afa
Mehdi Lahmam <mehdi@craftsmen.io>
2014-09-22 10:16:21 +0200

    Update routes to connect only the used ones

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root to: 'home#show'
+
   devise_for :users
 
   resources :setup, only: :new
-  resources :groups, path: 'classes' do
-    resources :subscribers
+  resources :groups, except: :index, path: 'classes' do
+    resources :subscribers, only: [:new, :create]
     resources :messages, only: :create
   end
 end
